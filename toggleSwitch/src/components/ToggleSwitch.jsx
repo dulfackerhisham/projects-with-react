5e9c8f3c413d241d97d3be42ef9e34e012ae628c
Hisham Dulfacker <dulfackerhisham@gmail.com>
2025-06-17 13:18:06 +0530

    used logical not operator for better code and simplicity

--- toggleSwitch/src/components/ToggleSwitch.jsx
+++ toggleSwitch/src/components/ToggleSwitch.jsx
@@ -4,10 +4,11 @@ export const ToggleSwitch = () => {
   const [open, setOpen] = useState(true);
 
   return (
-    <div className="text-center mt-32">
+    <div className="text-center mt-32 border-2">
       <button
-        className={`${open ? "bg-green-600" : "bg-red-600"} p-10`}
-        onClick={open ? () => setOpen(false) : () => setOpen(true)}
+        className={`${open ? "bg-green-600" : "bg-red-600"} p-10 cursor-pointer`}
+        // onClick={open ? () => setOpen(false) : () => setOpen(true)}
+        onClick={() => setOpen(!open)} //logical not operator
       >
         {open ? "ON" : "OFF"}
       </button>
