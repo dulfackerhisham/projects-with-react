f402afa09d83342bde3d61a0ba094c102e52c748
Hisham Dulfacker <dulfackerhisham@gmail.com>
2025-06-17 13:30:27 +0530

    used derived state for the value

--- toggleSwitch/src/components/ToggleSwitch.jsx
+++ toggleSwitch/src/components/ToggleSwitch.jsx
@@ -3,14 +3,16 @@ import { useState } from "react";
 export const ToggleSwitch = () => {
   const [open, setOpen] = useState(true);
 
+  const offBtn = open ? "ON" : "OFF";
+
   return (
     <div className="text-center mt-32 border-2">
       <button
-        className={`${open ? "bg-green-600" : "bg-red-600"} p-10 cursor-pointer`}
+        className={`${open ? "bg-green-600" : "bg-red-600"} p-10 cursor-pointer text-lg font-bold text-blue-800`}
         // onClick={open ? () => setOpen(false) : () => setOpen(true)}
         onClick={() => setOpen(!open)} //logical not operator
       >
-        {open ? "ON" : "OFF"}
+        {offBtn}
       </button>
     </div>
   );
